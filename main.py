de8d79a1cdba76da7b71c3027290ccf40cb065ec
Bryan Eckard <ch13f_48@zoho.com>
2023-12-30 15:19:36 -0500

    Add chapter 2 and 3 story code

--- main.py
+++ main.py
@@ -30,22 +30,6 @@ def start_adventure():
     print("Your mission is to find the treasure...")
     print("but beware, death is around every corner!")
     print("\n")
-    print('''
-           mm###########mmm
-        m####################m
-      m#####`"#m m###"""'######m
-     ######*"  "   "   "mm#######
-   m####"  ,             m"#######m
-  m#### m*" ,'  ;     ,   "########m
-  ####### m*   m  |#m  ;  m ########
- |######### mm#  |####  #m##########|
-  ###########|  |######m############
-  "##########|  |##################"
-   "#########  |## /##############"
-     ########|  # |/ m###########
-      "#######      ###########"
-        """"""       """""""""
-      ''')
 
     # Chapter 1: The Mysterious Arrival
     print('''Zandar washes up onto a deserted island. His eyes flutter open. He's on a beach, but how did he get here? The last thing he remembers is his ship being swallowed by a monstrous storm. He slowly stands, brushing sand off his tattered clothes. In front of him lies a dense jungle; behind, the endless ocean. A wooden sign catches his eye. It reads: "Welcome to Sudden Death Island. Beware the choices you make. Treasure beyond measure may be found by one brave, cunning, and lucky enough to succeed. Will Zandar accept the challenge? Does he explore the jungle or wait for rescue on the beach?''')
@@ -58,5 +42,36 @@ def start_adventure():
     else:
         print("Invalid choice. Please type 'explore' or 'wait'.")
         start_adventure()
+
+# Chapter 2: Into the Jungle
+def chapter2():
+    print('Venturing into the jungle, Zandar finds a fork in the path. A parrot squaks overhead, "Left leads to treasure, right to despair!"')
+    print("Zandar comes to a fork in the path. Does he go left or right?")
+    choice = input("Type 'left' or 'right': ").lower()
+
+    if choice == "left":
+        chapter4()
+    elif choice == "right":
+        chapter5()
+    else:
+        print("Invalid choice. Please type 'left' or 'right'.")
+        chapter2()
+
+# Chapter 3: The Beach's Secret
+def chapter3():
+    print("Zandar waits. Hours pass. As the sun sets, he notices a glint in the sand. It's a key! He finds a chest buried nearby. Inside, a map of the island with a path marked in red.")
+    print("Zandar finds a key and a map. Does he follow the map or build a raft?")
+    choice = input("Type 'map' to follow the map or 'raft' to build a raft: ").lower()
+
+    if choice == "map":
+        chapter6()
+    elif choice == "raft":
+        chapter7()
+    else:
+        print("Invalid choice. Please type 'map' or 'raft'.")
+        chapter3()
+
+
+
 
 start_adventure()
