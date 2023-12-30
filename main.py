e4b04ee0a64ff9f2cda97c48320d417a46ccc23f
Bryan Eckard <ch13f_48@zoho.com>
2023-12-30 15:57:59 -0500

    Add code for chapter 4, 5, and end game

--- main.py
+++ main.py
@@ -71,7 +71,37 @@ def chapter3():
         print("Invalid choice. Please type 'map' or 'raft'.")
         chapter3()
 
+# Chapter 4: The Left Path
+def chapter4():
+    print("The left path leads Zandar to a cave. Inside, a riddle on the wall reads: 'I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I?")
+    choice = input("Type your answer (you must spell correctly): ").lower()
+
+    if choice == "echo":
+        end_game("Correctly guessing 'an echo', a hidden door opens reavealing a room filled with treasure! Zandar has found the riches of Sudden Death Island!")
+    else:
+        end_game("Unable to solve the riddle, the cave collapses trapping Zandar. With no means of escape, Zandar knows his fate is sealed...")
+
+# Chapter 5: The Right Path
+def chapter5():
+    print("The right path leads to a cliff. Below, sharp rocks. A vine hangs nearby. It could lead to the other side, or it might not hold his weight.")
+    print("Zandar faces a cliff. Does he swing across or turn back?")
+    choice = input("Type 'swing' to swing across or 'back' to turn back: ").lower()
+
+    if choice == "swing":
+        # Chapter 10: The Leap of Faith
+        end_game("Zandar swings across the chasm and lands safely. On the other side, he finds the treasure!")
+    elif choice == "back":
+        print("Zandar wisely returns to the fork and takes the left path.")
+        chapter4()
+    else:
+        print("Invalid choice. Please type 'swing' or 'back'.")
+        chapter5
 
 
+
+def end_game(message):
+    print("\n" + message)
+    print("The End")
+    exit()
 
 start_adventure()
